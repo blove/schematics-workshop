e3391bd6490bfffd68060fdb51e07e1e025187f2
Brian Love <brian@brianflove.com>
2019-04-27 14:31:59 -0600

    Context

--- src/schematic-starter/index.ts
+++ src/schematic-starter/index.ts
@@ -1,9 +1,15 @@
 import { Rule, SchematicContext, Tree } from "@angular-devkit/schematics";
 
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
 export function schematicStarter(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
+    // todo: use _context.logger.debug()
+
+    // todo: use _context.logger.info()
+
+    // todo: use _context.logger.warn()
+
+    // todo: use _context.logger.error()
+
     return tree;
   };
 }
