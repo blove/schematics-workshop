46b41d9d5c403d870b2b1f1d356d883ec7276976
kevin schuchard <schuchard.kevin@gmail.com>
2019-04-27 12:33:26 -0600

    add files, prompts

--- src/schematic-starter/index.ts
+++ src/schematic-starter/index.ts
@@ -1,9 +1,54 @@
-import { Rule, SchematicContext, Tree } from "@angular-devkit/schematics";
+import {
+  Rule,
+  SchematicContext,
+  Tree,
+  chain,
+  SchematicsException,
+  apply,
+  url,
+  template,
+  move,
+  mergeWith
+} from "@angular-devkit/schematics";
+import { strings } from "@angular-devkit/core";
+import { getBase64Image } from "./utils";
+import { dasherize } from "@angular-devkit/core/src/utils/strings";
 
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
-export function schematicStarter(_options: any): Rule {
+interface AddFilesInterface {
+  name: string;
+  browserType: string;
+  selector: string;
+}
+
+export function schematicStarter(_options: AddFilesInterface): Rule {
+  _options = {
+    ..._options,
+    ...{ selector: `app-${dasherize(_options.name)}`, style: "css" }
+  };
+
   return (tree: Tree, _context: SchematicContext) => {
-    return tree;
+    return chain([addFiles(_options)])(tree, _context);
+  };
+}
+
+function addFiles(_options: AddFilesInterface): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    if (!_options.name) {
+      throw new SchematicsException("Entity name is required");
+    }
+    const path = `./src/app/components`;
+
+    context.logger.debug(`adding files to ${path} dir`);
+    _options.browserType = getBase64Image(_options.browserType);
+
+    const templateSource = apply(url("./files"), [
+      template({
+        ...strings,
+        ..._options
+      }),
+      move(path)
+    ]);
+
+    return chain([mergeWith(templateSource)])(tree, context);
   };
 }
